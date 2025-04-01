0c8afba301e629b13baff4ab3af895480f76f53c
Kaki309 <andres_gab.fernandez@uao.edu.co>
2025-03-31 20:23:02 -0500

    SuperJump

--- Assets/Andres/PlayerMovement.cs
+++ Assets/Andres/PlayerMovement.cs
@@ -1,10 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class PlayerMovement: MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] Transform orientation;
+    public TMP_Text texto;
 
     [Header("Movement")]
     float horizontalInput;
@@ -21,64 +23,123 @@ public class PlayerMovement: MonoBehaviour
     bool isGrounded;
     bool readyToJump = true;
 
+    [Header("Super Jump")]
+    [SerializeField] float superJumpMultiplier = 2f;
+    [SerializeField] float superJumpDuration = 5f;
+    [SerializeField] float superJumpCooldown = 20f;
+    bool canActivateSuperJump = false;
+    bool isSuperJumpActive = false;
+    float superJumpTimer = 0f;
+    float superJumpCooldownTimer = 0f;
+
     void Start()
     {
-        //Variables initialization
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
-    {   
+    {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        // Check Ground
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight/2 + 0.2f, whatIsGround);
-        //Debug.DrawRay(transform.position, Vector3.down * (playerHeight/2 + 0.2f), Color.red);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.2f, whatIsGround);
 
-        // Floor friction
         if (isGrounded)
             rb.drag = groundDrag;
         else
-            rb.drag=0;
-        
-        // Limit Speed
+            rb.drag = 0;
+
         speedControl();
 
-        // Jump
         if (Input.GetKeyDown(KeyCode.Space)) jump();
+
+        // Powers
+        HandleSuperJump();
+
+        // Debug
+        texto.text = $"Tiempo cooldown:{superJumpCooldownTimer}\nTiempo activo:{superJumpTimer}";
     }
+
     void FixedUpdate()
     {
-       move();
+        move();
     }
-    void move(){
+
+    void move()
+    {
         Vector3 moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         if (isGrounded)
             rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
-        else if (!isGrounded)
+        else
             rb.AddForce(moveDir.normalized * moveSpeed * airMultiplier * 10f, ForceMode.Force);
     }
-    void speedControl(){
+
+    void speedControl()
+    {
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        
-        if (flatVel.magnitude > moveSpeed){
+
+        if (flatVel.magnitude > moveSpeed)
+        {
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
     }
-    void jump(){
-        
-        if (readyToJump && isGrounded){
+
+    void jump()
+    {
+        if (readyToJump && isGrounded)
+        {
             rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
             readyToJump = false;
-            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+            float currentJumpForce = isSuperJumpActive ? jumpForce * superJumpMultiplier : jumpForce;
+            rb.AddForce(transform.up * currentJumpForce, ForceMode.Impulse);
             Invoke(nameof(resetJump), jumpCooldown);
         }
     }
-    void resetJump(){
+
+    void resetJump()
+    {
         readyToJump = true;
+    }
+
+    void HandleSuperJump()
+    {
+        if (!canActivateSuperJump)
+        {
+            superJumpCooldownTimer += Time.deltaTime;
+            if (superJumpCooldownTimer >= superJumpCooldown)
+            {
+                canActivateSuperJump = true;
+                superJumpCooldownTimer = 0f;
+            }
+        }
+
+        if (canActivateSuperJump && Input.GetKeyDown(KeyCode.E))
+        {
+            ActivateSuperJump();
+        }
+
+        if (isSuperJumpActive)
+        {
+            superJumpTimer += Time.deltaTime;
+            if (superJumpTimer >= superJumpDuration)
+            {
+                DeactivateSuperJump();
+            }
+        }
+    }
+
+    void ActivateSuperJump()
+    {
+        isSuperJumpActive = true;
+        canActivateSuperJump = false;
+        superJumpTimer = 0f;
+    }
+
+    void DeactivateSuperJump()
+    {
+        isSuperJumpActive = false;
     }
 }
