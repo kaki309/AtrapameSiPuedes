58ad33fde595b7e8ab85eb3eb133b8b61783ba51
NicoDiazFk <166237442+NicoDiazFk@users.noreply.github.com>
2025-05-28 13:39:17 -0500

    MENU TERMINADO

---
+++ Assets/Nicolas/MainMenuController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Panel de Instrucciones")]
+    [SerializeField] private GameObject instruccionesPanel;
+
+    public void Jugar()
+    {
+        SceneManager.LoadScene("Nivel1");
+    }
+
+    public void Instrucciones()
+    {
+        if (instruccionesPanel != null)
+            instruccionesPanel.SetActive(true);
+    }
+
+    public void CerrarInstrucciones()
+    {
+        if (instruccionesPanel != null)
+            instruccionesPanel.SetActive(false);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
+
+
